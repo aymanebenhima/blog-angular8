3469f2aae6112e14197edbb2f9df621af3a0cc66
Aymane Benhima <aymanebenhima@gmail.com>
2020-01-09 17:38:45 +0100

    article

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Article } from '../models/article';
 
 
 @Injectable({
@@ -12,5 +13,11 @@ export class ArticleService {
   getAll() {
 
     return this.afs.collection('articles').valueChanges();
+  }
+
+  saveArticle(data: Article) {
+    
+    return this.afs.collection('articles').add(data);
+
   }
 }
