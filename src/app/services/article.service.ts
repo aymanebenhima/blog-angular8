1b29fead14053d10c9b6797bee2bf21e9eeb0662
Aymane Benhima <aymanebenhima@gmail.com>
2020-01-09 15:19:49 +0100

    add collection

--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArticleService {
 
-  constructor() { }
+  constructor(private afs: AngularFirestore) { }
+
+  getAll() {
+
+    return this.afs.collection('articles').valueChanges();
+  }
 }
