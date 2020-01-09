5f37239c19c3cb8d7884dad4a5b018dc31586108
Aymane Benhima <aymanebenhima@gmail.com>
2020-01-09 11:34:20 +0100

    add routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
+import { PageNotFoundComponent } from './components/shared/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/backoffice/login/login.component';
+import { RegisterComponent } from './components/backoffice/register/register.component';
+import { ArticlesEditComponent } from './components/backoffice/articles-edit/articles-edit.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ArticlesListComponent } from './components/backoffice/articles-list/articles-list.component';
+import { ArticlesAddComponent } from './components/backoffice/articles-add/articles-add.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "blog", component: ArticlesListComponent },
+  { path: "blog/create", component: ArticlesAddComponent },
+  { path: "blog/:id/edit", component: ArticlesEditComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "login", component: LoginComponent },
+  { path: "**", component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
